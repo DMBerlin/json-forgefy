7e4d1ce7c9725ab70b2d387e6e5248ee195b2467
Daniel Marinho <contato@danielmarinho.dev>
2024-05-04 18:41:37 -0300

    chore: add test to is-object helper

---
+++ src/helpers/is-object.helper.spec.ts
@@ -0,0 +1,15 @@
+import { isObject } from "@helpers/is-object.helper";
+
+describe("Is Object Helper Test", () => {
+  it("should return true if the value is an object", () => {
+    expect(isObject({})).toBe(true);
+  });
+
+  it("should return false if the value is not an object", () => {
+    expect(isObject([])).toBe(false);
+    expect(isObject("")).toBe(false);
+    expect(isObject(0)).toBe(false);
+    expect(isObject(null)).toBe(false);
+    expect(isObject(undefined)).toBe(false);
+  });
+});
