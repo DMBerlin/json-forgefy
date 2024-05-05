8ab607c7c18d777b567687813d75d9851adad605
Daniel Marinho <danmg22@gmail.com>
2024-05-04 21:34:32 -0300

    Update gitignore and add package-lock.json (#2)
    * chore: update readme
    
    * chore: increment new tests
    
    * chore: add package lock
    
    * chore: update gitignore and pipeline
    
    * chore: update gitignore and pipeline
    
    * chore: add test to forgefy

--- src/core/forgefy.core.ts
+++ src/core/forgefy.core.ts
@@ -4,20 +4,44 @@ import { isObject } from "../helpers/is-object.helper";
 import { isOperator } from "../helpers/is-operator.common";
 import { resolveOperation } from "../common/resolve-operation.common";
 
+function assignValueByOperator(
+  key: string,
+  source: Record<string, any>,
+  blueprint: Record<string, any>,
+): void {
+  blueprint[key] = resolveOperation(source, blueprint[key]);
+}
+
+function assignValueByPath(
+  key: string,
+  source: Record<string, any>,
+  blueprint: Record<string, any>,
+): void {
+  blueprint[key] = getValueByPath(source, blueprint[key]);
+}
+
+function keyHandler(
+  key: string,
+  source: Record<string, any>,
+  blueprint: Record<string, any>,
+): void {
+  if (isValidObjectPath(blueprint[key])) {
+    assignValueByPath(key, source, blueprint);
+  } else if (isObject(blueprint[key])) {
+    if (isOperator(blueprint[key])) {
+      assignValueByOperator(key, source, blueprint);
+    } else {
+      blueprint[key] = forgefy(source, blueprint[key]);
+    }
+  }
+}
+
 export function forgefy(
   source: Record<string, any>,
   blueprint: Record<string, any>,
 ): Record<string, any> {
   for (const key of Object.keys(blueprint)) {
-    if (isValidObjectPath(blueprint[key])) {
-      blueprint[key] = getValueByPath(source, blueprint[key]);
-    } else if (isObject(blueprint[key])) {
-      if (isOperator(blueprint[key])) {
-        blueprint[key] = resolveOperation(source, blueprint[key]);
-      } else {
-        blueprint[key] = forgefy(source, blueprint[key]);
-      }
-    }
+    keyHandler(key, source, blueprint);
   }
   return blueprint;
 }
